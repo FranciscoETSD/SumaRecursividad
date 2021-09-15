2fd3a6cb49846b0f2315d8f0728fff25c8c6c900
FranciscoETSD <82955011+FranciscoETSD@users.noreply.github.com>
2021-09-15 09:41:31 -0400

    Add files via upload
    Suma Recursividad

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //.out.println(Ejercicios.factorial(5));
+
+        //System.out.println(Ejercicios.mcd(412, 184));
+        int[] vector = {4, 4, 6};
+        System.out.println(Ejercicios.sumaVector(vector, 0));
+    }
+}
